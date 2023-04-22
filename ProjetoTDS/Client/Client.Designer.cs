eb0a9bb1e2c0b275f1b64bcbed99b9527e8cac80
THETHUNDER8 <48048814+THETHUNDER8@users.noreply.github.com>
2023-04-22 15:15:05 +0100

    Add files via upload

---
+++ ProjetoTDS/Client/Client.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.labelMensagemEnviada = new System.Windows.Forms.Label();
+            this.listBoxMensagemEnviada = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(84, 240);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(151, 13);
+            this.labelMessage.TabIndex = 0;
+            this.labelMessage.Text = "Texto a enviar para o servidor:";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(87, 256);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(287, 62);
+            this.textBoxMessage.TabIndex = 1;
+            this.textBoxMessage.TextChanged += new System.EventHandler(this.textBoxMessage_TextChanged);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(414, 256);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Enviar";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(414, 295);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuit.TabIndex = 3;
+            this.buttonQuit.Text = "Sair";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // labelMensagemEnviada
+            // 
+            this.labelMensagemEnviada.AutoSize = true;
+            this.labelMensagemEnviada.Location = new System.Drawing.Point(84, 70);
+            this.labelMensagemEnviada.Name = "labelMensagemEnviada";
+            this.labelMensagemEnviada.Size = new System.Drawing.Size(151, 13);
+            this.labelMensagemEnviada.TabIndex = 4;
+            this.labelMensagemEnviada.Text = "Texto enviado para o servidor:";
+            // 
+            // listBoxMensagemEnviada
+            // 
+            this.listBoxMensagemEnviada.FormattingEnabled = true;
+            this.listBoxMensagemEnviada.Location = new System.Drawing.Point(87, 103);
+            this.listBoxMensagemEnviada.Name = "listBoxMensagemEnviada";
+            this.listBoxMensagemEnviada.Size = new System.Drawing.Size(287, 121);
+            this.listBoxMensagemEnviada.TabIndex = 5;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 363);
+            this.Controls.Add(this.listBoxMensagemEnviada);
+            this.Controls.Add(this.labelMensagemEnviada);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.labelMessage);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Client_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Label labelMensagemEnviada;
+        private System.Windows.Forms.ListBox listBoxMensagemEnviada;
+    }
+}
+
---
+++ ProjetoTDS/Client/Client.cs
@@ -0,0 +1,123 @@
+﻿using EI.SI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Client : Form
+    {
+        private const int PORT = 10000;
+        private byte[] key;
+        private byte[] iv;
+        NetworkStream networkStream;
+        ProtocolSI protocolSI;
+        TcpClient client;
+        AesCryptoServiceProvider aes;
+
+        public Client()
+        {
+            InitializeComponent();
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, PORT);
+            client = new TcpClient();
+            client.Connect(endpoint);
+            networkStream = client.GetStream();
+            protocolSI = new ProtocolSI();
+        }
+
+        private string GerarChavePrivada(string pass)
+        {
+            byte[] salt = new byte[] { 0, 1, 0, 8, 3, 9, 9, 7 };
+
+            Rfc2898DeriveBytes pwdGen = new Rfc2898DeriveBytes(pass, salt, 1000);
+
+            byte[] key = pwdGen.GetBytes(16);
+            string keyB64 = Convert.ToBase64String(key);
+            return keyB64;
+        }
+
+        private string GerarIV(string pass)
+        {
+            byte[] salt = new byte[] { 1, 9, 3, 4, 1, 0, 5, 8 };
+            Rfc2898DeriveBytes pwdGen = new Rfc2898DeriveBytes(pass, salt, 1000);
+
+            byte[] iv = pwdGen.GetBytes(16);
+            string iv64 = Convert.ToBase64String(iv);
+            return iv64;
+        }
+
+        private string CifrarTexto(string txt)
+        {
+            byte[] textoDecifrado = Encoding.UTF8.GetBytes(txt);
+            byte[] textoCifrado;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(textoDecifrado, 0, textoDecifrado.Length);
+            cs.Close();
+            textoCifrado = ms.ToArray();
+            string textoCifradoB64 = Convert.ToBase64String(textoCifrado);
+            return textoCifradoB64;
+        }
+
+        private string DefifrarTexto(string textoCifradoB64)
+        {
+            byte[] texoCifrado = Convert.FromBase64String(textoCifradoB64);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            byte[] textoDecifrado = new byte[ms.Length];
+            int bytesLidos = 0;
+            bytesLidos = cs.Read(textoDecifrado, 0, textoDecifrado.Length);
+            cs.Close();
+            string txtDecifrado = Encoding.UTF8.GetString(textoDecifrado, 0, textoDecifrado.Length);
+            return txtDecifrado;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string msg = textBoxMessage.Text;
+            textBoxMessage.Clear();
+            byte[] packet = protocolSI.Make(ProtocolSICmdType.DATA, msg);
+            networkStream.Write(packet, 0, packet.Length);
+            while (protocolSI.GetCmdType() != ProtocolSICmdType.ACK)
+            {
+                networkStream.Read(protocolSI.Buffer, 0, protocolSI.Buffer.Length);
+            }
+        }
+        
+        private void CloseClient()
+        {
+            byte[] eot = protocolSI.Make(ProtocolSICmdType.EOT);
+            networkStream.Write(eot, 0, eot.Length);
+            networkStream.Read(protocolSI.Buffer, 0, protocolSI.Buffer.Length);
+            networkStream.Close();
+            client.Close();
+        }
+                        //Exit
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            CloseClient();
+            this.Close();
+        }
+
+
+        private void Client_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseClient();
+        }
+
+        private void textBoxMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
